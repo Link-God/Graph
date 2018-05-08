bcafe50e30f3fb25b3b5c90ccbe3be958fa693b3
Yury <32190920+Link-God@users.noreply.github.com>
2018-05-08 22:15:36 +0300

    Update and rename tree.hpp to Graph.hpp

---
+++ include/Graph.hpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<sstream>
+
+using namespace std;
+
+class Graph {
+private:
+	vector < vector<unsigned> > graph;
+	vector<bool> used;
+	vector<unsigned> result;
+	unsigned N;
+public:
+	Graph();
+	Graph(unsigned n);
+	void read();
+	~Graph();
+	Graph(Graph & other);
+	Graph operator = (Graph & other);
+	void dfs(unsigned index);
+	void return_result(ostream & stream);
+};
